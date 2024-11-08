17db5c96ca8b110fcb801d3a7572bae16c41d05f
Chris Schuster <schustercp@gmail.com>
2024-11-08 16:30:10 +0000

    Added Rotating Cumpass.

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ class NaviBall
 {
   private:
   double displayWidth = 720.0;
+  double displayHeight = 720.0;
   double offset = displayWidth * 0.01388888;
   double halfWidth = (displayWidth / 2.0);
   double outerBand0 = (halfWidth - offset) * (1335.0 / 1335.0);
@@ -68,215 +69,38 @@ class NaviBall
   double depth_port = 0.0;
   double depth_starboard = 0.0;
 
-  // void drawInnerCircle()
-  // { 
-  //   printf("outerBand0: %f\n", outerBand0);
-  //   printf("outerBand1: %f\n", outerBand1);
-  //   printf("outerBand2: %f\n", outerBand2);
-
-  //   printf("innerBand0: %f\n", innerBand0);
-  //   printf("innerBand1: %f\n", innerBand1);
-  //   printf("innerBand2: %f\n", innerBand2);
-
-  //   /* set color for rectangle */
-  //   // cairo_set_source_rgb(cr, 0.0, 0.5, 0.5);
-  //   /* set the line width */
-  //   // cairo_set_line_width(cr, 1);
-  //   /* draw the rectangle's path beginning at 3,3 */
-  //   // cairo_arc(cr, centerX, centerY, outerBand0, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);
-  //   // cairo_arc(cr, centerX, centerY, outerBand1, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);	
-  //   // cairo_arc(cr, centerX, centerY, outerBand2, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);
-  //   // cairo_arc(cr, centerX, centerY, innerBand0 + 5, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);
-  //   // cairo_arc(cr, centerX, centerY, innerBand1, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);
-  //   // cairo_arc(cr, centerX, centerY, innerBand2, 0.0, 2.0*G_PI);
-  //   // cairo_stroke(cr);
-  // }
-
 /***************************************************************************************************/
-  void drawHeadingText()
-  {
+  void drawHeadingText() {
     cairo_set_font_size (cr, 30.0);
-    
-    double headingBand = outerBand2 - 35;
-    // cairo_set_source_rgb(cr, 0.0, 0.5, 0.5);
-    // cairo_set_line_width(cr, 1);
-    // cairo_arc(cr, centerX, centerY, headingBand, 0.0, 2.0*G_PI);
-    // cairo_stroke(cr);
-
-    drawText("N", centerX, centerY - (headingBand + 16), 0.0);
-    drawText("S", centerX, centerY + (headingBand + 16), 0.0);
-
-    //Starboard Numbers/
-
-    double X = centerX + ((headingBand + 24) * cos( 61.0 * G_PI / 180.0 ));
-    double Y = centerY - ((headingBand + 24) * sin( 61.0 * G_PI / 180.0 ));
-    drawText("30", X, Y, 30.0);
-
-    X = centerX + ((headingBand + 28) * cos( 33.0 * G_PI / 180.0 ));
-    Y = centerY - ((headingBand + 28) * sin( 33.0 * G_PI / 180.0 ));
-    drawText("60", X, Y, 60.0);
-
-    X = centerX + (headingBand + 15); // 16 - > 15
-    Y = centerY - 19; //5 -> 15 -> 18
-    drawText("E", X, Y, 90.0);
-
-    X = centerX + ((headingBand + 48) * cos( 31.0 * G_PI / 180.0 ));
-    Y = centerY + ((headingBand + 48) * sin( 31.0 * G_PI / 180.0 ));
-    drawText("120", X, Y, -60);
-
-    X = centerX + ((headingBand + 34) * cos( 59.5 * G_PI / 180.0 ));
-    Y = centerY + ((headingBand + 34) * sin( 59.5 * G_PI / 180.0 ));
-    drawText("150", X, Y, -30);
-
-    //Port Numbers
-
-    X = centerX - ((headingBand - 2) * cos( 60.0 * G_PI / 180.0 )); //0 -> -2
-    Y = centerY - ((headingBand - 2) * sin( 60.0 * G_PI / 180.0 ));
-    drawText("330", X, Y, -30.0);
-
-    X = centerX - ((headingBand - 17) * cos( 29.0 * G_PI / 180.0 )); //14 -> 16
-    Y = centerY - ((headingBand - 17) * sin( 29.0 * G_PI / 180.0 ));
-    drawText("300", X, Y, -60.0);
-
-    X = centerX - (headingBand) + 12;
-    Y = centerY + 7;
-    drawText("W", X, Y, 270.0);
-
-    X = centerX - ((headingBand - 4) * cos( 25.0 * G_PI / 180.0 ));
-    Y = centerY + ((headingBand - 4) * sin( 25.0 * G_PI / 180.0 ));
-    drawText("240", X, Y, 60.0);
-
-    X = centerX - ((headingBand + 3) * cos( 58.0 * G_PI / 180.0 ));
-    Y = centerY + ((headingBand + 3) * sin( 58.0 * G_PI / 180.0 ));
-    drawText("210", X, Y, 30.0);
-  }
-
-/***************************************************************************************************/
-  void drawAWAText()
-  {
-    cairo_set_font_size (cr, 20.0);
-
-    drawText("0", centerX, centerY - (innerBand0 + 16), 0.0);
-    drawText("180", centerX, centerY + (innerBand0 + 20), 0.0);
-
-    //Starboard Numbers/
-
-    double X = centerX + ((innerBand0 + 25) * cos( 61.0 * G_PI / 180.0 ));
-    double Y = centerY - ((innerBand0 + 25) * sin( 61.0 * G_PI / 180.0 ));
-    drawText("30", X, Y, 30.0);
-
-    X = centerX + ((innerBand0 + 30) * cos( 33.0 * G_PI / 180.0 ));
-    Y = centerY - ((innerBand0 + 30) * sin( 33.0 * G_PI / 180.0 ));
-    drawText("60", X, Y, 60.0);
-
-    X = centerX + (innerBand0 + 20);
-    Y = centerY - 20;
-    drawText("90", X, Y, 90.0);
-
-    X = centerX + ((innerBand0 + 50) * cos( 31.0 * G_PI / 180.0 ));
-    Y = centerY + ((innerBand0 + 50) * sin( 31.0 * G_PI / 180.0 ));
-    drawText("120", X, Y, -60);
-
-    X = centerX + ((innerBand0 + 35) * cos( 59.5 * G_PI / 180.0 ));
-    Y = centerY + ((innerBand0 + 35) * sin( 59.5 * G_PI / 180.0 ));
-    drawText("150", X, Y, -30);
-
-    //Port Numbers
-
-    X = centerX - ((innerBand0 + 5) * cos( 60.0 * G_PI / 180.0 ));
-    Y = centerY - ((innerBand0 + 5) * sin( 60.0 * G_PI / 180.0 ));
-    drawText("30", X, Y, -30.0);
-
-    X = centerX - ((innerBand0 - 8) * cos( 30.0 * G_PI / 180.0 ));
-    Y = centerY - ((innerBand0 - 8) * sin( 30.0 * G_PI / 180.0 ));
-    drawText("60", X, Y, -60.0);
-
-    X = centerX - (innerBand0) + 12;
-    Y = centerY + 10;
-    drawText("90", X, Y, 270.0);
-
-    X = centerX - ((innerBand0) * cos( 25.0 * G_PI / 180.0 ));
-    Y = centerY + ((innerBand0) * sin( 25.0 * G_PI / 180.0 ));
-    drawText("120", X, Y, 60.0);
-
-    X = centerX - ((innerBand0 + 6) * cos( 58.0 * G_PI / 180.0 ));
-    Y = centerY + ((innerBand0 + 6) * sin( 58.0 * G_PI / 180.0 ));
-    drawText("150", X, Y, 30.0);
-  }
-
-/***************************************************************************************************/
-  void drawText(const char* text, double x, double y, double angle)
-  {
-    cairo_font_extents_t fe;
-	  cairo_text_extents_t te;
-    double xAdj;
-    double yAdj;
-    // double px;
-    // double ux=1.0;
-    // double uy=1.0;
-
-    // cairo_font_options_t* fontOptions = cairo_font_options_create();
-    // cairo_font_options_set_custom_palette_color(fontOptions,
-    //                             1,
-    //                             49.0/255.0,
-    //                             96.0/255.0,
-    //                             91.0/255.0,
-    //                             1.0);
-    // cairo_font_options_set_color_mode(fontOptions, CAIRO_COLOR_MODE_COLOR);
-    // cairo_font_options_set_color_palette(fontOptions, 1);
-
-    /* Drawing code goes here */
     cairo_select_font_face (cr, "Georgia", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL /*CAIRO_FONT_WEIGHT_BOLD*/);
-    //cairo_set_font_options(cr, fontOptions);
 
-    // cairo_device_to_user_distance (cr, &ux, &uy);
-    // if (ux > uy)
-    //   px = ux;
-    // else
-    //   px = uy;
-    cairo_font_extents (cr, &fe);
-    cairo_text_extents (cr, text, &te);
-    xAdj = x - te.x_bearing - te.width / 2;
-    yAdj = y - fe.descent + fe.height / 2;
+    cairo_translate(cr, centerX, centerY);
 
-    // printf("cairo_text_extents of %s:\n", text);
-    // printf("\txBearing: %f\n", te.x_bearing);
-    // printf("\tyBearing: %f\n", te.y_bearing);
-    // printf("\twidth: %f\n", te.width);
-    // printf("\theight: %f\n", te.height);
-    // printf("\txAdvance: %f\n", te.x_advance);
-    // printf("\tyAdvance: %f\n", te.y_advance);
-
-    // printf("cairo_font_extents of %s:\n", text);
-    // printf("\tascent: %f\n", fe.ascent);
-    // printf("\tdescent: %f\n", fe.descent);
-    // printf("\theight: %f\n", fe.height);
-    // printf("\tmax_x_advance: %f\n", fe.max_x_advance);
-    // printf("\tmax_y_advance: %f\n", fe.max_y_advance);
-
-    // printf("Text X: %f, Y: %f - xAdj: %f, yAdj: %f\n",x,y,xAdj,yAdj);
-
-    /* text */
-    // cairo_move_to(cr, xAdj, yAdj);
-    // cairo_rotate(cr, angle * G_PI / 180.0); // Convert angle to radians
-    // cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
-    // cairo_show_text(cr, text);                   
-
-    cairo_move_to(cr, xAdj, yAdj);
-    cairo_rotate(cr, angle * G_PI / 180.0); // Convert angle to radians
-    cairo_text_path(cr, text);
-    cairo_set_source_rgb(cr, 101.0/255.0, 171.0/255.0, 156.0/255.0);
-    
-    cairo_fill_preserve(cr);
-    //cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
-    cairo_set_source_rgb(cr, 49.0/255.0, 96.0/255.0, 91.0/255.0);
-    cairo_set_line_width(cr, 0.5);
-    cairo_stroke(cr);
+    const double offsetFromBand = outerBand2 - 20;
+    cairo_rotate(cr, (0.0 + heading_sat) * G_PI / 180.0);
+    drawText("N",   0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("30",  0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("60",  0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("E",   0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("120", 0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("150", 0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("S",   0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("210", 0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("240", 0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("W",   0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("300",  0, -offsetFromBand, 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("330",  0, -offsetFromBand, 0.0);
 
     cairo_identity_matrix(cr); //Reset the CTM
     cairo_move_to(cr, centerX, centerY);
@@ -284,11 +108,70 @@ class NaviBall
   }
 
 /***************************************************************************************************/
-  void drawRadiaLines()
-  {
+  void drawAWAText() {
+    cairo_set_font_size (cr, 20.0);
+    cairo_select_font_face (cr, "Georgia", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL /*CAIRO_FONT_WEIGHT_BOLD*/);
+
+    cairo_translate(cr, centerX, centerY);
+
+    const double offsetFromBand = 12.0;
+    drawText("0",   0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("30",  0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("60",  0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("90",  0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("120", 0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("150", 0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("180", 0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("150", 0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("120", 0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("90",  0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("60",  0, -(innerBand0 + offsetFromBand), 0.0);
+    cairo_rotate(cr, 30.0 * G_PI / 180.0);
+    drawText("30",  0, -(innerBand0 + offsetFromBand), 0.0);
+
+    cairo_identity_matrix(cr); //Reset the CTM
+    cairo_move_to(cr, centerX, centerY);
+    cairo_stroke(cr);
+  }
+
+/***************************************************************************************************/
+  void drawText(const char* text, double x, double y, double angle) {
+    cairo_font_extents_t fe;
+	  cairo_text_extents_t te;
+    double xAdj;
+    double yAdj;
+    
+    //Adjust for size of the Text.
+    cairo_font_extents (cr, &fe);
+    cairo_text_extents (cr, text, &te);
+    xAdj = x - te.x_bearing - (te.width / 2);
+    yAdj = y - fe.descent + (fe.height / 2);
+
+    //Fill first then draw an outline to make the text crisp
+    cairo_move_to(cr, xAdj, yAdj);
+    cairo_text_path(cr, text);
+    cairo_set_source_rgb(cr, 101.0/255.0, 171.0/255.0, 156.0/255.0);
+    cairo_fill_preserve(cr);
+    cairo_set_source_rgb(cr, 49.0/255.0, 96.0/255.0, 91.0/255.0);
+    cairo_set_line_width(cr, 0.5);
+    cairo_stroke(cr);
+  }
+
+/***************************************************************************************************/
+  void drawRadiaLines() {
     //double radius = (halfWidth - offset);
 
-    //Draw out Band Gradient
+    //Draw outer Band Gradient
     double rad = outerBand2;
     double rad2 = innerBand0 + 10;
     cairo_pattern_t* radialGradient = cairo_pattern_create_radial(centerX, centerY, rad, centerX, centerY, rad2);
@@ -298,36 +181,43 @@ class NaviBall
     cairo_arc(cr, centerX, centerY, rad, 0.0, 2.0 * G_PI);
     cairo_fill(cr);
 
-    /* set color for rectangle */
+    //Draw outer Circle of hash marks.
+    // set color
     cairo_set_source_rgb(cr, 0.0, 0.5, 0.5);
-    /* set the line width */
+    // set the line width
     cairo_set_line_width(cr, 1);
 
+    //Rotate in a circle dawing the minor hash marks
     for( int deg = 0; deg < 360; deg += 1)
     {
-      double X = cos( deg * oneRadian );
-      double Y = sin( deg * oneRadian );
+      double X = cos( (deg + heading_sat) * oneRadian );
+      double Y = sin( (deg + heading_sat) * oneRadian );
       cairo_move_to(cr, X * outerBand1 + centerX, Y * outerBand1 + centerY);
       cairo_line_to(cr, X * outerBand0 + centerX, Y * outerBand0 + centerY);
 
+      X = cos( deg * oneRadian );
+      Y = sin( deg * oneRadian );
       cairo_move_to(cr, X * innerBand1 + centerX, Y * innerBand1 + centerY);
       cairo_line_to(cr, X * innerBand0 + centerX, Y * innerBand0 + centerY);
     }
     cairo_stroke(cr);
 
-    /* set color for rectangle */
+    //Draw inner Circle of hash marks.
+    // set color
     cairo_set_source_rgb(cr, 0.0, 0.5, 0.5);
-    /* set the line width */
+    // set the line width
     cairo_set_line_width(cr, 2);
 
+    //Rotate in a circle dawing the semi-major hash marks
     for( int deg = 0; deg < 360; deg += 5)
     {
-      double X = cos( deg * oneRadian );
-      double Y = sin( deg * oneRadian );
-
+      double X = cos( (deg + heading_sat) * oneRadian );
+      double Y = sin( (deg + heading_sat) * oneRadian );
       cairo_move_to(cr, X * outerBand1 + centerX, Y * outerBand1 + centerY);
       cairo_line_to(cr, X * outerBand0 + centerX, Y * outerBand0 + centerY);
 
+      X = cos( deg * oneRadian );
+      Y = sin( deg * oneRadian );
       cairo_move_to(cr, X * innerBand1 + centerX, Y * innerBand1 + centerY);
       cairo_line_to(cr, X * innerBand0 + centerX, Y * innerBand0 + centerY);
     }
@@ -338,14 +228,16 @@ class NaviBall
     /* set the line width */
     cairo_set_line_width(cr, 2);
 
+    //Rotate in a circle dawing the major hash marks
     for( int deg = 0; deg < 360; deg += 10)
     {
-      double X = cos( deg * oneRadian );
-      double Y = sin( deg * oneRadian );
-
+      double X = cos( (deg + heading_sat) * oneRadian );
+      double Y = sin( (deg + heading_sat) * oneRadian );
       cairo_move_to(cr, X * outerBand2 + centerX, Y * outerBand2 + centerY);
       cairo_line_to(cr, X * outerBand0 + centerX, Y * outerBand0 + centerY);
 
+      X = cos( deg * oneRadian );
+      Y = sin( deg * oneRadian );
       cairo_move_to(cr, X * innerBand2 + centerX, Y * innerBand2 + centerY);
       cairo_line_to(cr, X * innerBand0 + centerX, Y * innerBand0 + centerY);
     }
@@ -369,8 +261,7 @@ class NaviBall
   }
 
   /***************************************************************************************************/
-  void drawAWAIndicator()
-  {
+  void drawAWAIndicator() {
     double width = 26;
     double height = 50;
     cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
@@ -389,39 +280,25 @@ class NaviBall
 
     cairo_identity_matrix(cr); //Reset the CTM
   }
+
+  void Draw()
+  {
+    /* Set color for background */
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    /* fill in the background color*/
+    cairo_paint(cr);
+
+    drawRadiaLines();
+    drawAWAText();
+    drawHeadingText();
+    drawAWAIndicator();
+  }
 };
 
 NaviBall naviBall;
 
 /***************************************************************************************************/
-static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data)
-{
-  /* Set color for background */
-  cairo_set_source_rgb(cr, 0, 0, 0);
-  /* fill in the background color*/
-  cairo_paint(cr);
-     
-  /* set color for rectangle */
-  //cairo_set_source_rgb(cr, 0.42, 0.65, 0.80);
-  /* set the line width */
-  //cairo_set_line_width(cr,6);
-  /* draw the rectangle's path beginning at 3,3 */
-  //cairo_rectangle (cr, 3, 3, 100, 100);
-  /* stroke the rectangle's path with the chosen color so it's actually visible */
-  //cairo_stroke(cr);
-
-  /* draw circle */
-  //cairo_set_source_rgb(cr, 0.17, 0.63, 0.12);
-  //cairo_set_line_width(cr,2);
-  //cairo_arc(cr, 150, 210, 20, 0, 2*G_PI);
-  //cairo_stroke(cr);
-
-  /* draw horizontal line */
-  //cairo_set_source_rgb(cr, 0.77, 0.16, 0.13);
-  //cairo_set_line_width(cr, 6);
-  //cairo_move_to(cr, 80,160);
-  //cairo_line_to(cr, 200, 160);
-  //cairo_stroke(cr);
+static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data) {
 
   if(naviBall.widget == NULL)
   {
@@ -430,17 +307,13 @@ static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data)
     naviBall.data = data;
   }
 
-  naviBall.drawRadiaLines();
-  naviBall.drawAWAText();
-  naviBall.drawHeadingText();
-  naviBall.drawAWAIndicator();
+  naviBall.Draw();
 
   return FALSE;
 }
 
 /***************************************************************************************************/
-gboolean key_callback ( GtkWidget *widget, GdkEventKey *event )
-{
+gboolean key_callback ( GtkWidget *widget, GdkEventKey *event ) {
     ( void ) widget;
     if ( event->state & GDK_CONTROL_MASK )
     {
@@ -467,7 +340,7 @@ gboolean timer_exe(GtkWidget * window)
   {
     naviBall.heading_sat = 0.0;
   }
-  naviBall.heading_sat += 0.01;
+  naviBall.heading_sat += 0.2;
 
   if(naviBall.aparentWindAngle < 0.0)
   {
